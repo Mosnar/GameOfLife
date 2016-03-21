93f050f0b926a7cb05da204e5b44d72e7c0b784b
Ransom <ranmantwo@gmail.com>
2016-03-21 15:04:17 -0400

    Got gridlines drawing

--- app/scripts/GameOfLife.js
+++ app/scripts/GameOfLife.js
@@ -14,82 +14,57 @@
         width: 500,
         height: 380,
 
-        canvas: null
+        canvas: null,
+        ctx: null
     };
 
+    /**
+     * Called on page load - initalizes the GameOfLife board
+     */
     GameOfLife.init = function () {
         this.canvas = document.getElementById("canv");
-        console.log(this.canvas);
+        this.ctx = this.canvas.getContext('2d');
+
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+
+        this.drawGrid();
     };
+
+    /**
+     * 
+     */
+    GameOfLife.drawGrid = function () {
+        var cellWidth = this.width / this.col;
+        var cellHeight = this.height / this.row;
+
+        for (var x = cellWidth; x < this.width; x += cellWidth) {
+            this.ctx.moveTo(x, 0);
+            this.ctx.lineTo(x, this.width);
+        }
+
+        for (var y = cellHeight; y < this.height; y += cellHeight) {
+            this.ctx.moveTo(0, y);
+            this.ctx.lineTo(this.width, y);
+        }
+
+        this.ctx.strokeStyle = "#ddd";
+        this.ctx.stroke();
+    };
+
+    // GameOfLife.drawCells = function () {
+    //     var cellWidth = width / xCells;
+    //     var cellHeight = height / yCells;
+    //     ctx.fillStyle = 'white';
+    //     ctx.fillRect(0, 0, cellWidth, cellHeight);
+    // };
 
     // Event Listeners //
 
     // Initialize GameOfLife once the page is done loading
     window.addEventListener('load', GameOfLife.init(), false);
-    //     this.ctx = canvas.getContext("2d");
-    //
-    //     var width = 500;
-    //     var height = 380;
-    //
-    //     this.setHeight = function (height) {
-    //         this.height = height;
-    //         return this;
-    //     };
-    //
-    //     this.setWidth = function (width) {
-    //         this.width = width;
-    //         return this;
-    //     };
-    //
-    //     this.getHeight = function () {
-    //         return this.height;
-    //     };
-    //
-    //     this.getWidth = function () {
-    //         return this.width;
-    //     };
-    // };
 })();
 
-//
-//
-// var app = {
-//     width: 500,
-//     height: 380,
-//
-//     xCells: 20,
-//     yCells: 20,
-//
-//     setWidth: function (width) {
-//         this.width = width;
-//     },
-//
-//     setHeight: function (height) {
-//         this.height = height;
-//     },
-//
-//     drawGrid: function () {
-//         var cellWidth = width / xCells;
-//         var cellHeight = height / yCells;
-//
-//         for (var x = cellWidth; x < this.width; x += cellWidth) {
-//             ctx.moveTo(x, 0);
-//             ctx.lineTo(x, this.width);
-//         }
-//
-//         for (var y = cellHeight; y < this.height; y += cellHeight) {
-//             ctx.moveTo(0, y);
-//             ctx.lineTo(this.width, y);
-//         }
-//     },
-//
-//     drawCells: function () {
-//         var cellWidth = width / xCells;
-//         var cellHeight = height / yCells;
-//         ctx.fillStyle = 'white';
-//         ctx.fillRect(0, 0, cellWidth, cellHeight);
-//     }
-// };
 //
 // drawGrid(width, height, xCells, yCells);
 // drawCells();
