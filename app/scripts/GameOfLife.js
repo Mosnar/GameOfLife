cf3490ebc71e497779ac5512d1925d9a1edca17b
Ransom <ranmantwo@gmail.com>
2016-03-21 15:46:06 -0400

    Got mouse handler running

--- app/scripts/GameOfLife.js
+++ app/scripts/GameOfLife.js
@@ -8,14 +8,19 @@
  */
 (function () {
     var GameOfLife = {
-        col: 10,
-        row: 10,
+        col: 20,
+        row: 20,
 
         width: 500,
         height: 380,
 
         canvas: null,
-        ctx: null
+        ctx: null,
+
+        cfgDrawGrid: true,
+
+        helpers: {},
+        eventHandlers: {}
     };
 
     /**
@@ -28,11 +33,20 @@
         this.width = this.canvas.width;
         this.height = this.canvas.height;
 
-        this.drawGrid();
+        if (this.cfgDrawGrid) {
+            this.drawGrid();
+        }
+
+        this.drawCells();
+
+        this.canvas.addEventListener('mousemove', this.eventHandlers.mouseMove.bind(this), true);
+        this.helpers.parent = this;
+        this.self = this;
     };
 
+
     /**
-     * 
+     *
      */
     GameOfLife.drawGrid = function () {
         var cellWidth = this.width / this.col;
@@ -52,22 +66,50 @@
         this.ctx.stroke();
     };
 
-    // GameOfLife.drawCells = function () {
-    //     var cellWidth = width / xCells;
-    //     var cellHeight = height / yCells;
-    //     ctx.fillStyle = 'white';
-    //     ctx.fillRect(0, 0, cellWidth, cellHeight);
-    // };
+    GameOfLife.drawCells = function () {
+        var cellWidth = this.width / this.col;
+        var cellHeight = this.height / this.row;
+        this.ctx.fillStyle = '#fff';
+        this.ctx.fillRect(0, 0, cellWidth, cellHeight);
+    };
+
+    GameOfLife.handleMouseMove = function(pos) {
+        console.log(pos);
+    };
+
+    GameOfLife.eventHandlers.mouseMove = function (evt) {
+        this.handleMouseMove(this.helpers.translateMousePos(this.helpers.getRelativeMousePos(evt)));
+    };
+
+    /**
+     * Computes and returns the relative mouse position with the top-left being 0,0
+     * @param evt
+     * @returns {{x: number, y: number}}
+     */
+    GameOfLife.helpers.getRelativeMousePos = function(evt) {
+        var rect = this.parent.canvas.getBoundingClientRect();
+        var x = evt.clientX - rect.left;
+        var y = evt.clientY - rect.top;
+        return {x: x, y: y};
+    };
+
+    /**
+     * Translates relative mouse coordinates into cell coordinates
+     * @param pos
+     * @returns {{x: number, y: number}}
+     */
+    GameOfLife.helpers.translateMousePos = function(pos) {
+        var cellWidth = this.parent.width / this.parent.col;
+        var cellHeight = this.parent.height / this.parent.row;
+
+        var xPos = Math.floor(pos.x / cellWidth);
+        var yPos = Math.floor(pos.y / cellHeight);
+
+        return {x: xPos, y: yPos};
+    };
+
 
     // Event Listeners //
-
     // Initialize GameOfLife once the page is done loading
     window.addEventListener('load', GameOfLife.init(), false);
 })();
-
-//
-// drawGrid(width, height, xCells, yCells);
-// drawCells();
-// ctx.strokeStyle = "#ddd";
-// ctx.stroke();
-//
